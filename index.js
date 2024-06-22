05d3d49b18b525f39a24632013267b28050b4235
daviiisousa <157535081+daviiisousa@users.noreply.github.com>
2024-06-22 16:57:13 -0300

    mudandos o estilo e add id

--- index.js
+++ index.js
@@ -3,34 +3,38 @@ async function API(){
         method: "GET"
     };
 
-    let resposta = document.getElementById('res')
+    let resposta = document.getElementById('res');
 
    const response = await fetch("https://rickandmortyapi.com/api/character", metodos);
    const data = await response.json();
 
    console.log(data);
 
-   const personagens = data.results
+   const personagens = data.results;
 
    personagens.map((personagem) =>{
     const div = document.createElement('div');
+
+    const id = document.createElement('p');
+    id.innerText = personagem.id;
 
     const p = document.createElement('p');
     p.innerText = personagem.name;
 
     const gender = document.createElement('p')
-    gender.innerText = personagem.gender
+    gender.innerText = personagem.gender;
 
     const imagen = document.createElement('img')
-    imagen.src =personagem.image
+    imagen.src =personagem.image;
 
     const especie = document.createElement('p')
-    especie.innerText = personagem.species
+    especie.innerText = personagem.species;
    
     div.appendChild(imagen);
+    div.appendChild(id);
     div.appendChild(p);
-    div.appendChild(gender)
-    div.appendChild(especie)
+    div.appendChild(gender);
+    div.appendChild(especie);
     
 
     resposta.appendChild(div)
